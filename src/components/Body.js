1bb742c205f710f31d0bea5a2fba559a13df345e
Kanchan Kadam <kanchanvkadam@gmail.com>
2023-08-23 15:19:16 +0530

    Login-Logout

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,14 +3,14 @@ import resList from "../utils/mockData";
 import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
 
-const Body = () =>{
-    const [restaurantList, setRestaurantList] =useState([]);
+const Body = () => {
+    const [restaurantList, setRestaurantList] = useState([]);
 
-    useEffect(()=>{
+    useEffect(() => {
         fetchData();
-    },[])
+    }, [])
 
-     const fetchData= async()=>{
+    const fetchData = async () => {
         const data = await fetch(
             "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
         )
@@ -19,30 +19,29 @@ const Body = () =>{
         updateedSwiggyAPI(json);
     }
 
-    const updateedSwiggyAPI = (json)=>{
-        json.data.cards.map((i)=>{
-            if(i.card.card.id ==='top_brands_for_you' ){
+    const updateedSwiggyAPI = (json) => {
+        json.data.cards.map((i) => {
+            if (i.card.card.id === 'top_brands_for_you') {
                 setRestaurantList(i?.card?.card?.gridElements?.infoWithStyle?.restaurants);
             }
         });
     }
 
-    if(restaurantList.length ===0){
-        return <Shimmer />
-    }
-
-    return(
+    //conditional rendering
+    return restaurantList.length === 0 ? (
+        <Shimmer />
+    ) : (
         <div className="body">
             <div className="filter">
-            <button  className="filter-btn" onClick={() =>{
-                const filterList = restaurantList.filter((res)=> res?.info?.avgRating>4);
-                setRestaurantList(filterList);
-            }}>Top Rated Restaurants</button>
+                <button className="filter-btn" onClick={() => {
+                    const filterList = restaurantList.filter((res) => res?.info?.avgRating > 4);
+                    setRestaurantList(filterList);
+                }}>Top Rated Restaurants</button>
             </div>
             <div className="res-container">
-            {restaurantList.map(i=>
-                <RestaurantCard key={i.info?.id} resData = {i} />
-            )}
+                {restaurantList.map(i =>
+                    <RestaurantCard key={i.info?.id} resData={i} />
+                )}
             </div>
         </div>
     )
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,10 @@
+import { useState } from "react";
 import { LOGO_URL } from "../utils/constants.js";
 
 const Header =()=>{
+    const [btnNameReact, setbtnNameReact] = useState("Login");
     return(
-        <div className="header">
+        <div className="header">""
             <div className="logo">
                 <img className="image" src={LOGO_URL} />
             </div>
@@ -12,6 +14,10 @@ const Header =()=>{
                     <li>About Us</li>
                     <li>Contact Us</li>
                     <li>Cart</li>
+                    <button className="login" onClick={()=>{
+                        btnNameReact === "Login"?setbtnNameReact("Logout"):setbtnNameReact("Login");
+                        console.log(btnNameReact);
+                    }}>{btnNameReact}</button>
                 </ul>
             </div>
         </div>
